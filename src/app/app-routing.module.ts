946c405f89ae222313113f9ce6ac0693f18e3a51
Harry Rutter <hrutter@global-aero.com>
2022-09-23 09:27:02 +0100

    Fix before test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
 	{ path: "", redirectTo: "/dashboard", pathMatch: "full" },
 	{ path: "dashboard", component: DashboardComponent },
 	{ path: "detail/:id", component: HeroDetailComponent },
-	{ path: "this-is-not-the-correct-path", component: HeroesComponent },
+	{ path: "heroes", component: HeroesComponent },
 ];
 
 @NgModule({
