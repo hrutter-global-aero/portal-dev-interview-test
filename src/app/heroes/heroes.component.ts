9c93f26dced8b20531c3af582093ce96b1ab840c
Harry Rutter <hrutter@global-aero.com>
2022-10-06 11:23:56 +0100

    Oops

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -35,7 +35,7 @@ export class HeroesComponent implements OnInit {
 
 	/**
 	 * Call delete hero service and update the page to reflect the changes.
-	*/
+	 */
 	delete(hero: Hero): void {
 		// Call delete hero service passing the ID and subscribing to the observable.
 		this.heroService.deleteHero(hero.id).subscribe();
